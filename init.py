64404b92da7f3bb459db901e3cac8321684d9bf7
alexrsg1253 <alexrsg2003@gmail.com>
2026-07-07 14:51:58 +0300

    Create init.py

---
+++ init.py
@@ -0,0 +1,15 @@
+from .schwarzschild import (
+    qnms,
+    converged_qnm,
+    hill_det,
+    matrix,
+    recur_coeffs,
+)
+
+__all__= [
+    "qnms",
+    "converged_qnm",
+    "hill_det",
+    "matrix",
+    "recur_coeffs",
+]
